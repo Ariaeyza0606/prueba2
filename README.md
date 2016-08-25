e967231b3ce8280f541c837e49efa22de5809f0b
ariaeyza0606 <ariadna.eyzaguirre@gmail.com>
2016-08-25 18:22:50 -0500

    diagrama fluid

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# prueba2
+# Prueba2
+
+Inicio
+Numero
+n%2==0 (no)-> n es impar
+(si)
+n es par
+fin
+![alt](http://4.1m.yt/uSDb97U.jpg)
---
+++ app.js
@@ -0,0 +1,7 @@
+var num = parseInt(prompt ("Ingrese un numero"));
+if(num%2==0){
+    document.write ("Par");
+}
+else{
+    document.write("Impar");
+}
